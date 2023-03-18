72ae2ea25edff342100029233eefe43aa051709a
Bogdan <bogdan.oloeriu@gmail.com>
2023-03-18 13:17:57 +0100

    added hapijs fix

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ import App from './src/App';
 import HelloApp from './src/HelloApp';
 import HelloNavigation from './src/HelloNavigation';
 import {name as appName} from './app.json';
+import 'text-encoding-polyfill'
+import Joi from '@hapi/joi'
 
 AppRegistry.registerComponent(appName, () => HelloNavigation);
