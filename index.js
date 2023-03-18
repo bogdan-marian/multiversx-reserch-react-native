762a898e87c1c454b4e2871d1dc1d48f90ccf63b
Bogdan <bogdan.oloeriu@gmail.com>
2023-03-18 09:54:32 +0100

    added hello home

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import HelloApp from './src/HelloApp';
+import HelloNavigation from './src/HelloNavigation';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => HelloApp);
+AppRegistry.registerComponent(appName, () => HelloNavigation);
