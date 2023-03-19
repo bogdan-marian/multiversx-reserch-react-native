668949bfe0957db632f0b6403ab9e21357880e43
Bogdan <bogdan.oloeriu@gmail.com>
2023-03-19 08:50:57 +0100

    added hello connect v2

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ import App from './src/App';
 import HelloApp from './src/HelloApp';
 import HelloNavigation from './src/HelloNavigation';
 import {name as appName} from './app.json';
+import HelloConnectV2 from './src/HelloConnectV2';
 
-AppRegistry.registerComponent(appName, () => HelloNavigation);
+AppRegistry.registerComponent(appName, () => HelloConnectV2);
---
+++ src/HelloConnectV2.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const HelloConnectV2 = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello, ConnectV2</Text>
+    </View>
+  );
+};
+export default HelloConnectV2;
